92d4f78f7b82f1dadd3efeef4b38ac46618d5d81
Aaron Walsman <awalsman@nvidia.com>
2018-06-27 14:40:19 -0700

    adding turntable camera pose

--- renderpy/camera.py
+++ renderpy/camera.py
@@ -16,3 +16,40 @@ def projection_matrix(
             [0, 0, -(far_clip + near_clip) / (far_clip - near_clip),
                 -2 * far_clip * near_clip / (far_clip - near_clip)],
             [0, 0, -1, 0]])
+
+def turntable_pose(
+        distance, orientation, elevation, spin):
+    
+    d = numpy.array([
+            [1,0,0,0],
+            [0,1,0,0],
+            [0,0,1,distance],
+            [0,0,0,1]])
+    
+    ce = math.cos(elevation)
+    se = math.sin(elevation)
+    e = numpy.array([
+            [1, 0, 0, 0],
+            [0, ce, -se, 0],
+            [0, se, ce, 0],
+            [0, 0, 0, 1]])
+    
+    co = math.cos(orientation)
+    so = math.sin(orientation)
+    o = numpy.array([
+            [co, 0, -so, 0],
+            [0, 1, 0, 0],
+            [so, 0, co, 0],
+            [0, 0, 0, 1]])
+    
+    cs = math.cos(spin)
+    ss = math.sin(spin)
+    s = numpy.array([
+            [cs, -ss, 0, 0],
+            [ss, cs, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1]])
+    
+    return numpy.linalg.inv(
+            numpy.dot(o, numpy.dot(e, numpy.dot(d,s))))
+
