57f7684d67dda89075dbf882d471be80cd6939f8
Aaron Walsman <awalsman@nvidia.com>
2018-07-25 17:51:25 -0700

    trying again at shadow maps

--- renderpy/shader_definitions.py
+++ renderpy/shader_definitions.py
@@ -7,6 +7,12 @@ layout(location=2) in vec2 vertex_uv;
 out vec4 fragment_position;
 out vec4 fragment_normal;
 out vec2 fragment_uv;
+out vec4 fragment_shadow_position;
+out vec4 fragment_shadow_normal;
+out vec4 fragment_shadow_projection;
+
+uniform mat4 shadow_light_pose;
+uniform mat4 shadow_light_projection;
 
 uniform mat4 projection_matrix;
 uniform mat4 model_pose;
@@ -17,12 +23,20 @@ void main(){
     mat4 vm = camera_pose * model_pose;
     mat4 pvm = projection_matrix * vm;
     
+    mat4 lm = shadow_light_pose * model_pose;
+    
     gl_Position = pvm * vec4(vertex_position,1);
     
     fragment_position = vm * vec4(vertex_position,1);
     fragment_normal = vm * vec4(vertex_normal,0);
     
-    fragment_uv = vertex_uv;
+    fragment_uv.x = vertex_uv.x;
+    fragment_uv.y =-vertex_uv.y;
+    
+    fragment_shadow_position = lm * vec4(vertex_position,1);
+    fragment_shadow_projection =
+            shadow_projection_matrix * fragment_shadow_position;
+    fragment_shadow_normal = lm * vec4(vertex_normal,0);
 }
 '''
 
@@ -33,6 +47,9 @@ const int MAX_NUM_LIGHTS = 8;
 in vec4 fragment_position;
 in vec4 fragment_normal;
 in vec2 fragment_uv;
+in vec4 fragment_shadow_position;
+in vec4 fragment_shadow_projection;
+in vec4 fragment_shadow_normal;
 
 out vec3 color;
 
@@ -41,12 +58,41 @@ uniform vec4 material_properties;
 uniform vec3 ambient_color;
 uniform int num_point_lights;
 uniform int num_direction_lights;
+uniform int enable_shadow_light;
+uniform vec3 shadow_light_color;
+//uniform mat4 shadow_light_pose;
+//uniform mat4 shadow_light_projection;
 uniform vec3 point_light_data[2*MAX_NUM_LIGHTS];
 uniform vec3 direction_light_data[2*MAX_NUM_LIGHTS];
 
 uniform mat4 camera_pose;
 
 uniform sampler2D texture_sampler;
+uniform sampler2D shadow_sampler;
+
+vec2 phong(
+        vec3 contact_position,
+        vec3 contact_normal,
+        vec3 light_position,
+        vec3 eye_direction){
+    
+    vec3 light_direction = concact_position - light_position;
+    float light_distance = length(light_direction);
+    light_direction = light_direction / light_length;
+    
+    float diffuse = clamp(
+            dot(-light_direction, contact_normal), 0, 1);
+    
+    vec3 reflected_direction = reflect(
+            -light_direction, contact_normal);
+    float specular = clamp(
+            dot(-eye_direction, reflected_direction), 0, 1);
+    if(specular > 0.0){
+        specular = pow(specular, shine);
+    }
+    
+    return vec2(diffuse, specular);
+}
 
 void main(){
     
@@ -62,6 +108,26 @@ void main(){
     vec3 eye_ray = normalize(vec3(-fragment_position));
     
     vec3 fragment_normal_n = normalize(vec3(fragment_normal));
+    
+    if(enable_shadow_light){
+        // light position is 0,0,0 in shadow space
+        vec3 light_direction = vec3(fragment_shadow_position)
+        float light_distance = length(light_direction)
+        light_direction = light_direction / light_distance;
+        
+        fragment_shadow_normal_n = normalize(vec3(fragment_shadow_normal));
+        
+        vec3 reflected_direction = reflect(
+                -light_direction, fragment_shadow_normal_n);
+        float diffuse = clamp(
+                dot(-light_direction, fragment_shadow_normal_n), 0, 1);
+        float specular = clamp(dot(-shadow_eye_ray, reflected_direction), 0, 1);
+        if(specular > 0.0){
+            specular = pow(specular, shine);
+        }
+        diffuse_contribution += shadow_light_color * diffuse;
+        specular_contribution += shadow_light_color * specular;
+    }
     
     for(int i = 0; i < num_point_lights; ++i){
         vec3 light_color = vec3(point_light_data[2*i]);
