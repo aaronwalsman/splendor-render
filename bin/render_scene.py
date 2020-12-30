8becbe4bfe62403cce7817a923f662722d5f17af
Aaron Walsman <aaronwalsman@gmail.com>
2020-12-29 18:35:27 -0800

    cleaning, moving guts to viewer.py

--- bin/render_scene.py
+++ bin/render_scene.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
-import time
-import sys
 import argparse
-import numpy
 import renderpy.buffer_manager_egl as buffer_manager
-from renderpy import core
+from renderpy.core as core
 from renderpy.image import save_image, save_depth
 
+'''
 def render_scene(
         scene,
         width,
@@ -44,28 +42,30 @@ def render_scene(
     return image
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Render a Scene')
-    parser.add_argument('scene', type=str,
-            help='json file containing scene data')
-    parser.add_argument('output', type=str,
-            help='destination path for rendered image')
-    parser.add_argument('--dim', type=str, default='512x512',
-            help='dimensions of the output file in WIDTHxHEIGHT format')
-    parser.add_argument('--anti-alias', dest='anti_alias', action='store_true',
-            help='turn anti-alias on (default is on)')
-    parser.add_argument(
-            '--no-anti-alias', dest='anti_alias', action='store_false',
-            help='turn anti-alias off')
-    parser.set_defaults(anti_alias=True)
-    parser.add_argument('--render-mode', type=str, default='color',
-            help='should be either "color", "mask" or "depth"')
-    
-    args = parser.parse_args()
-    width, height = (int(wh) for wh in args.dim.split('x'))
-    render_scene(
-            args.scene,
-            width,
-            height, 
-            output_file = args.output,
-            anti_alias = args.anti_alias,
-            render_mode = args.render_mode)
+'''
+
+parser = argparse.ArgumentParser(description='Render a Scene')
+parser.add_argument('scene', type=str,
+        help='json file containing scene data')
+parser.add_argument('output', type=str,
+        help='destination path for rendered image')
+parser.add_argument('--resolution', type=str, default='512x512',
+        help='dimensions of the output file in WIDTHxHEIGHT format')
+parser.add_argument('--anti-alias', dest='anti_alias', action='store_true',
+        help='turn anti-alias on (default is on)')
+parser.add_argument(
+        '--no-anti-alias', dest='anti_alias', action='store_false',
+        help='turn anti-alias off')
+parser.set_defaults(anti_alias=True)
+parser.add_argument('--render-mode', type=str, default='color',
+        help='should be either "color", "mask" or "depth"')
+
+args = parser.parse_args()
+width, height = (int(wh) for wh in args.dim.split('x'))
+render_scene(
+        args.scene,
+        width,
+        height, 
+        output_file = args.output,
+        anti_alias = args.anti_alias,
+        render_mode = args.render_mode)
