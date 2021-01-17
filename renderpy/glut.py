3f6a7d7d39c28c2b7692cd0bef162423a3eaa8dd
Adam Fishman <afishman@cs.washington.edu>
2021-01-17 16:33:32 -0600

    Added OpenGL import wrapper for Mac support

--- renderpy/glut.py
+++ renderpy/glut.py
@@ -1,28 +1,26 @@
 import os
 
-from OpenGL.GL import *
-import OpenGL.GLUT as GLUT
-
 import numpy
 
+from renderpy.opengl_wrapper import GL, GLUT
 import renderpy.camera as camera
 
 glut_state = {
     'initialized' : False
 }
 
-def initialize_glut(x_authority = None):
+def initialize_glut(display=None, x_authority=None):
     if glut_state['initialized'] == False:
         if x_authority is not None:
             os.environ['XAUTHORITY'] = x_authority
             os.environ['DISPLAY'] = display
-        
+
         GLUT.glutInit([])
         glut_state['initialized'] = True
 
 def finish():
-    glFlush()
-    glFinish()
+    GL.glFlush()
+    GL.glFinish()
     GLUT.glutPostRedisplay()
     GLUT.glutSwapBuffers()
     GLUT.glutLeaveMainLoop()
@@ -35,13 +33,13 @@ class GlutWindowWrapper:
             height = 128,
             anti_alias = True,
             anti_alias_samples = 8):
-        
+
         self.name = name
         self.width = width
         self.height = height
         self.anti_alias = anti_alias
         self.anti_alias_samples = anti_alias_samples
-        
+
         GLUT.glutInit([])
         if self.anti_alias:
             GLUT.glutInitDisplayMode(
@@ -54,41 +52,41 @@ class GlutWindowWrapper:
         GLUT.glutInitWindowSize(self.width, self.height)
         self.window_id = GLUT.glutCreateWindow(name)
         self.set_active()
-    
+
     def hide_window(self):
         GLUT.glutHideWindow(self.window_id)
-    
+
     def show_window(self):
         GLUT.glutShowWindow(self.window_id)
-    
+
     def resize_window(self, width, height):
         if self.width != width or self.height != height:
             self.width = width
             self.height = height
             GLUT.glutReshapeWindow(width, height)
-    
+
     def set_active(self):
         GLUT.glutSetWindow(self.window_id)
-    
+
     def enable_window(self):
-        glBindFramebuffer(GL_FRAMEBUFFER, 0)
-        glViewport(0, 0, self.width, self.height)
+        GL.glBindFramebuffer(GL.GL_FRAMEBUFFER, 0)
+        GL.glViewport(0, 0, self.width, self.height)
         if self.anti_alias:
-            glEnable(GL_MULTISAMPLE)
+            GL.glEnable(GL.GL_MULTISAMPLE)
         else:
-            glDisable(GL_MULTISAMPLE)
-    
+            GL.glDisable(GL.GL_MULTISAMPLE)
+
     def read_pixels(self, read_depth = False, projection=None):
         self.enable_window()
         width = self.width
         height = self.height
         anti_alias = self.anti_alias
-        
+
         if read_depth:
             if projection is None:
                 raise ValueError('Must specify a projection when reading depth')
             near, far = camera.clip_from_projection(projection)
-            pixels = glReadPixels(
+            pixels = GL.glReadPixels(
                     0, 0, width, height, GL_DEPTH_COMPONENT, GL_UNSIGNED_SHORT)
             image = numpy.frombuffer(pixels, dtype=numpy.ushort).reshape(
                     height, width, 1)
@@ -96,14 +94,14 @@ class GlutWindowWrapper:
             image = 2.0 * image - 1.0
             image = 2.0 * near * far / (far + near - image * (far - near))
         else:
-            pixels = glReadPixels(
+            pixels = GL.glReadPixels(
                     0, 0, width, height, GL_RGB, GL_UNSIGNED_BYTE)
             image = numpy.frombuffer(pixels, dtype=numpy.uint8).reshape(
                     height, width, 3)
-        
-        glViewport(0, 0, width, height)
+
+        GL.glViewport(0, 0, width, height)
         return image
-    
+
     def start_main_loop(self, **callbacks):
         self.set_active()
         for callback_name, callback_function in callbacks.items():
