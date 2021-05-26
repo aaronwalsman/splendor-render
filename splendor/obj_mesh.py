88b99d4c5f83653b4448781898e4d4321b2eeb40
Aaron Walsman <aaronwalsman@gmail.com>
2021-05-25 21:00:50 -0700

    removing triangulate_obj.py script, moving the function into obj_mesh.py and making a bin script

--- splendor/obj_mesh.py
+++ splendor/obj_mesh.py
@@ -213,3 +213,25 @@ def write_obj(mesh, obj_path, texture_name=None):
         mtl.write('Ni 1.00\n')
         mtl.write('map_Kd %s\n'%texture_name)
 
+def triangulate_obj(in_path, out_path):
+    with open(os.path.expanduser(in_path)) as f:
+        lines = []
+        for line in f:
+            tokens = line.strip().split()
+            if not tokens:
+                lines.append(line)
+                continue
+            if tokens[0] == 'f':
+                if len(tokens) == 4:
+                    lines.append(line)
+                else:
+                    for i in range(len(tokens)-3):
+                        lines.append('f %s %s %s\n'%(
+                                tokens[1], tokens[i+2], tokens[i+3]))
+            else:
+                lines.append(line)
+
+    out = ''.join(lines)
+    with open(os.path.expanduser(out_path), 'w') as f:
+        f.write(out)
+
